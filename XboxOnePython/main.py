31c91defaddf8e328c4f3bbe666bf569ceffdfc6
Juan Sebastian <juans.cardenasg@autonoma.edu.co>
2023-09-25 00:01:26 -0500

    tcp dron

--- XboxOnePython/main.py
+++ XboxOnePython/main.py
@@ -1,12 +1,35 @@
 from evdev import ecodes
 import XboxInputController as xic
 import WifiPostEsp32 as wpe
+import threading
+import queue  
+
+# Número máximo de hilos permitidos
+max_threads = 8
+
+# Cola para encolar los datos a enviar al ESP32
+data_queue = queue.Queue(max_threads)
+
+def send_data_worker():
+    while True:
+        input = data_queue.get() 
+        if input is not None:
+            wpe.send_data_to_esp32(input)
+        data_queue.task_done()  
 
 def XboxInputController(device):
+    # Inicia el hilo para enviar datos
+    send_data_thread = threading.Thread(target=send_data_worker)
+    send_data_thread.start()
+
     for event in device.read_loop():
         input = xic.controllerListener(event)
         if input is not None:
-            wpe.send_data_to_esp32(input)
+            # Intenta agregar datos a la cola
+            try:
+                data_queue.put(input, block=False)
+            except queue.Full:
+                print("Cola de datos llena, espera para enviar más datos")
 
 def main():
     try:
